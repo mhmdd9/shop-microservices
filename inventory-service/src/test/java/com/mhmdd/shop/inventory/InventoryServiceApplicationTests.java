d400f14fed54fddd4c69c16f1c4559a179df6324
Mohamad <yusha.9ine@gmail.com>
2024-06-15 15:24:34 +0330

    wiremock and Spring cloud OpenFeign

--- inventory-service/src/test/java/com/mhmdd/shop/inventory/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/com/mhmdd/shop/inventory/InventoryServiceApplicationTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MySQLContainer;
 
--- order-service/src/main/java/com/mhmdd/shop/order/service/OrderService.java
+++ order-service/src/main/java/com/mhmdd/shop/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.mhmdd.shop.order.service;
 
+import com.mhmdd.shop.order.client.InventoryClient;
 import com.mhmdd.shop.order.dto.OrderRequest;
 import com.mhmdd.shop.order.model.Order;
 import com.mhmdd.shop.order.repository.OrderRepository;
@@ -13,13 +14,19 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        orderRepository.save(order);
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        if (isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
+        }
     }
 }
