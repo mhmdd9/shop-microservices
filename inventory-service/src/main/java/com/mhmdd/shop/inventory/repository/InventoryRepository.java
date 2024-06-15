282545bd305d772cff4e175216f2f1cff9415818
Mohamad <yusha.9ine@gmail.com>
2024-06-15 11:28:32 +0330

    first commit

---
+++ inventory-service/src/main/java/com/mhmdd/shop/inventory/repository/InventoryRepository.java
@@ -0,0 +1,8 @@
+package com.mhmdd.shop.inventory.repository;
+
+import com.mhmdd.shop.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, int quantity);
+}
