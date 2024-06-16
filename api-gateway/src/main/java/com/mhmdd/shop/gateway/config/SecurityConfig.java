c1fc80aa10bf6f9edc579dd91aa7201728f676ad
Mohamad <yusha.9ine@gmail.com>
2024-06-16 11:02:16 +0330

    restClient added (openFeign removed)

--- api-gateway/src/main/java/com/mhmdd/shop/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/mhmdd/shop/gateway/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.mhmdd.shop.gateway;
+package com.mhmdd.shop.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,10 +9,14 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
+            "/api-docs/**", "/swagger-resources/**", "/aggregate/**"};
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest()
-                .authenticated())
+        return httpSecurity.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(freeResourceUrls).permitAll()
+                        .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
